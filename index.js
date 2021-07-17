a2e37a83deced3d4e0d76052b68a69a1c659e882
naimur4062 <naimur4062@gmail.com>
2021-07-17 19:59:08 +0800

    update

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
-const ObjectId = require('mongodb').ObjectID;
-const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+const MongoClient = require('mongodb').MongoClient;
 const cors = require('cors');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const port = 5000
 
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mtqgx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
